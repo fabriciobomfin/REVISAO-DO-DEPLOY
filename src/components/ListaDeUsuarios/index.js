48d18e76847b7d022ccbca8620cec406719539da
Fabrício Bomfim <134095128+fabriciobomfin@users.noreply.github.com>
2025-06-02 10:42:19 -0300

    UPDATE

--- src/components/ListaDeUsuarios/index.js
+++ src/components/ListaDeUsuarios/index.js
@@ -10,7 +10,7 @@ function ListaDeUsuarios() {
     useEffect(() => {
         const carregarUsuarios = async () => {
             try {
-                const response = await axios.get('http://localhost:8080/usuarios')
+                const response = await axios.get('https://tcc-backand-senai.onrender.com/usuarios')
                 setUsuarios(response.data)
             } catch (error) {
                 alert('Erro ao buscar usuários: ', error)
